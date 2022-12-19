f1a15569e651ee7b56bcc6aa4ca2ff0f9ac27016
hersvik <hersvik@shoreline.no>
2022-12-19 13:41:54 +0100

    SignInScreen

--- beami-client/src/App.tsx
+++ beami-client/src/App.tsx
@@ -1,12 +1,32 @@
 import { useState } from 'react'
 import './App.css'
+// import { initializeApp } from "firebase/app";
+import SignInScreen from './SignInScreen';
 
 function App() {
+  // // Import the functions you need from the SDKs you need
+  // // TODO: Add SDKs for Firebase products that you want to use
+  // // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // // Your web app's Firebase configuration
+  // const firebaseConfig = {
+  //   apiKey: "AIzaSyDE6U13tAY7XTTkzN3nvjY05LJxWm-t1SQ",
+  //   authDomain: "beam-ahead.firebaseapp.com",
+  //   projectId: "beam-ahead",
+  //   storageBucket: "beam-ahead.appspot.com",
+  //   messagingSenderId: "240835648688",
+  //   appId: "1:240835648688:web:b9a6b9c2e7b942cdc7ef75"
+  // };
+
+  // // Initialize Firebase
+  // const app = initializeApp(firebaseConfig);
+
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
       <h1>Beami test deploy2</h1>
+      <SignInScreen />
     </div>
   )
 }
---
+++ beami-client/src/SignInScreen.tsx
@@ -0,0 +1,40 @@
+// Import FirebaseAuth and firebase.
+import React from 'react';
+import StyledFirebaseAuth from 'react-firebaseui/StyledFirebaseAuth';
+import firebase from 'firebase/compat/app';
+import 'firebase/compat/auth';
+
+// Configure Firebase.
+const config = {
+  apiKey: "AIzaSyDE6U13tAY7XTTkzN3nvjY05LJxWm-t1SQ",
+  authDomain: "beam-ahead.firebaseapp.com",
+  projectId: "beam-ahead",
+  storageBucket: "beam-ahead.appspot.com",
+  messagingSenderId: "240835648688",
+  appId: "1:240835648688:web:b9a6b9c2e7b942cdc7ef75"
+};
+firebase.initializeApp(config);
+
+// Configure FirebaseUI.
+const uiConfig = {
+  // Popup signin flow rather than redirect flow.
+  signInFlow: 'popup',
+  // Redirect to /signedIn after sign in is successful. Alternatively you can provide a callbacks.signInSuccess function.
+  signInSuccessUrl: '/signedIn',
+  // We will display Google and Facebook as auth providers.
+  signInOptions: [
+    firebase.auth.GoogleAuthProvider.PROVIDER_ID,
+  ],
+};
+
+function SignInScreen() {
+  return (
+    <div>
+      <h1>My App</h1>
+      <p>Please sign-in:</p>
+      <StyledFirebaseAuth uiConfig={uiConfig} firebaseAuth={firebase.auth()} />
+    </div>
+  );
+}
+
+export default SignInScreen
